8b472904209f368b5c594c8aef4882f96b1300df
Keith Flynn <flynn.keith@gmail.com>
2024-02-06 10:57:36 -0500

    Remove starter boilerplate and begin nav bar
    Stripped code from src/app/page.tsx and globals.css. Begin work on
    Navbar.

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Navbar({}: Props) {
+  return (
+    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
+      <div className="h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto"></div>
+      Navbar</nav>
+  )
+}
