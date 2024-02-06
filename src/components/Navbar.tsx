49c3b1473746d0ad7f20b182bb36636dc6b02bc1
Keith Flynn <flynn.keith@gmail.com>
2024-02-06 12:06:50 -0500

    Complete navbar visuals
    Add location icons, a search field and button. SearchBox needs props.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { MdWbSunny } from "react-icons/md";
+import { MdMyLocation } from "react-icons/md";
+import { MdOutlineLocationOn } from "react-icons/md";
+import SearchBox from './SearchBox';
 
 type Props = {}
 
@@ -11,6 +14,16 @@ export default function Navbar({}: Props) {
           <h2 className="text-gray-500 text-3xl">Weather</h2>
           <MdWbSunny className="text-3xl mt-1 text-yellow-300" />
         </p>
+        {/* */}
+        <section className="flex gap-2 items-center">
+          <MdMyLocation className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
+          <MdOutlineLocationOn className="text-3xl" />
+          <p className="text-slate-900/80 text-sm"> LocationName </p>
+          <div>
+            {/* SearchBox */}
+            <SearchBox />
+          </div>
+        </section>
       </div>
     </nav>
   )
