bfcff4606f28d7f219a8c785138ed98e24db1af2
Keith Flynn <flynn.keith@gmail.com>
2024-02-08 15:52:40 -0500

    Add TypeScript data types to page.tsx
    Manually call the endpoint in a browser with 2 results and paste the
    info to ChatGPT to create data types. Never occured to me before.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,66 @@
 import Image from "next/image";
 import Navbar from "@/components/Navbar";
 
+// http://api.openweathermap.org/data/2.5/forecast?q=${place}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=56
+
+type WeatherData = {
+  cod: string;
+  message: number;
+  cnt: number;
+  list: WeatherInfo[];
+  city: CityInfo;
+};
+
+type WeatherInfo = {
+  dt: number;
+  main: {
+      temp: number;
+      feels_like: number;
+      temp_min: number;
+      temp_max: number;
+      pressure: number;
+      sea_level: number;
+      grnd_level: number;
+      humidity: number;
+      temp_kf: number;
+  };
+  weather: {
+      id: number;
+      main: string;
+      description: string;
+      icon: string;
+  }[];
+  clouds: {
+      all: number;
+  };
+  wind: {
+      speed: number;
+      deg: number;
+      gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+      pod: string;
+  };
+  dt_txt: string;
+};
+
+type CityInfo = {
+  id: number;
+  name: string;
+  coord: {
+      lat: number;
+      lon: number;
+  };
+  country: string;
+  population: number;
+  timezone: number;
+  sunrise: number;
+  sunset: number;
+};
+
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
