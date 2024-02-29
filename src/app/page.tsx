55464b449dcb517208bb27bbcfc4541508b17ff4
Keith Flynn <flynn.keith@gmail.com>
2024-02-29 15:39:38 -0500

    Complete right frame of forecast summary
    Add all the components and props for the individual weather summary
    datum. Also alter tsconfig.json to add target = es2015 so that
    TypeScript is happy with the new Use function.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,6 +88,23 @@ export default function Home() {
 
   console.log("data", data)
 
+  const uniqueDates = [
+    ...new Set(
+      data?.list.map(
+        (entry) => new Date(entry.dt * 1000).toISOString().split("T")[0]
+      )
+    )
+  ];
+
+  // Filtering data to get the first entry after 6 AM for each unique date
+  const firstDataForEachDate = uniqueDates.map((date) => {
+    return data?.list.find((entry) => {
+      const entryDate = new Date(entry.dt * 1000).toISOString().split("T")[0];
+      const entryTime = new Date(entry.dt * 1000).getHours();
+      return entryDate === date && entryTime >= 6;
+    });
+  });
+
   if (isLoading) 
     return (
       <div className="flex items-center min-h-screen justify-center">
@@ -174,7 +191,32 @@ export default function Home() {
         {/* 7 day forecast data */}
         <section className="flex w-full flex-col gap-4">
           <p className="text-2xl">Forecast (7 days)</p>
-          <ForecastWeatherDetail />
+          {firstDataForEachDate.map((d, i) => (
+            <ForecastWeatherDetail 
+              key={i}
+              description={d?.weather[0].description ?? ""} 
+              weatherIcon={d?.weather[0].icon ?? "01d"} 
+              date={format(parseISO(d?.dt_txt ?? ""), "dd.MM")} 
+              day={format(parseISO(d?.dt_txt ?? ""), "EEEE")} 
+              feels_like={d?.main.feels_like ?? 0} 
+              temp={d?.main.temp ?? 0} 
+              temp_max={d?.main.temp_max ?? 0} 
+              temp_min={d?.main.temp_min ?? 0} 
+              airPressure={`${d?.main.pressure} hPa `} 
+              humidity={`${d?.main.humidity}% `} 
+              sunrise={format(
+                fromUnixTime(data?.city.sunrise ?? 1702517657),
+                "H:mm"
+              )}
+              sunset={format(
+                fromUnixTime(data?.city.sunset ?? 1702517657),
+                "H:mm"
+              )}
+              visibility={`${convertMetersToMiles(d?.visibility ?? 10000)} `} 
+              windSpeed={`${convertWindSpeed(d?.wind.speed ?? 1.64)} `}
+
+            />
+          ))}
         </section>
       </main>
     </div>
